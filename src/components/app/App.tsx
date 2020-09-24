1f418fd0e4544643772f47f23f1929aa3f951606
Anton Fresher <fresher@yandex-team.ru>
2020-09-24 21:10:18 +0300

    Fix structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Home from '../home';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {cn} from '@bem-react/classname';
+
+const home__ = cn('Home');
+
+export default function Home() {
+  return (
+    <div className={home__()}></div>
+  );
+}
