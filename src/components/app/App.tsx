0916bc3f20bf659977639d86fa8ea2c5d93373b8
Anton Fresher <fresher@yandex-team.ru>
2020-09-25 23:56:20 +0300

    Add YM

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {YMInitializer} from 'react-yandex-metrika';
 
 import Home from '../Home';
 
@@ -9,6 +10,11 @@ import './App.css';
 export default function App() {
     return (
         <Router>
+            <YMInitializer
+                accounts={[47324241]}
+                options={{webvisor: true}}
+                version="2"
+            />
             <Switch>
                 <Route path="/">
                     <Home />
